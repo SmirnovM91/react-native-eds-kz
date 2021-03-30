f5958c9198939be99c5e9b851069c80a36df23bd
Mikhail Smirnov <smirnov.m.n.91@gmail.com>
2021-03-30 23:01:31 +0600

    initial

---
+++ android/src/main/java/dev/amsmirnov/rnedskz/RnedskzPackage.java
@@ -0,0 +1,24 @@
+package dev.amsmirnov.rnedskz;
+  
+  import java.util.Arrays;
+  import java.util.Collections;
+  import java.util.List;
+  
+  import com.facebook.react.ReactPackage;
+  import com.facebook.react.bridge.NativeModule;
+  import com.facebook.react.bridge.ReactApplicationContext;
+  import com.facebook.react.uimanager.ViewManager;
+  import com.facebook.react.bridge.JavaScriptModule;
+  
+  public class RnedskzPackage implements ReactPackage {
+      @Override
+      public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+          return Arrays.<NativeModule>asList(new RnedskzModule(reactContext));
+      }
+  
+      @Override
+      public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+          return Collections.emptyList();
+      }
+  }
+  
---
+++ ios/Rnedskz.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface Rnedskz : NSObject <RCTBridgeModule>
+
+@end
